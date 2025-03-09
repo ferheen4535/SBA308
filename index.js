58198eca62301cdd9f1c1eb9eab4146ca6285f6c
ferheen4535 <ferheen.syed@gmail.com>
2025-03-09 16:15:15 -0400

    add5

--- index.js
+++ index.js
@@ -77,118 +77,50 @@ const CourseInfo = {
 
 ];
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-function getLearnerData(course, ag, submissions) {    /////DECLARING THE FUNCTION///// 
+
+
+
+function getLearnerData(course, ag, submissions)  {  /////DECLARING THE FUNCTION///// 
   /// course = 1,  ag = 2 , submissions = 3////
-let learners = {}   ///empty box to load learners info//
-////////////////INFO TO RUN THE FUNCITION- PRAMATERS /////////////////////////////
-let assignments = {};    //empty box to load assignments info//
-for (let assignment of ag.assignments) {                      ///2///
-     assignments[assignment.id] = assignment;
+
+  let learners = {},    ////empty box for results//
+
+for (const assignment of ag.assignments) {
+const mainSubmissions = submissions.filter (sub => sub.assignment_id === assignment.id);   ///using the filter method///
+
+for (const submission of mainSubmissions) {
+if (!learners[submission.learner_id]) {
+learners[submission.learner_id]; {
+  learners[submission.learner_id] = { id: submission.learner_id, totalScore: 0, totalPoints: 0};
+}
+ let score = submission.submission.score;
+ const isLate = new Date(submission.submission.submitted_at) > new Date(assignment.due_at);
+
+ if (isLate) { 
+  score * = 0.9;
+
+ }
+ learners[submission.learner_id] [assignment.id] = score / assignment.points_possible;
+ learners[submission.learner_id].totalScore += score;
+ learners[submission.learner_id].totalPoints += assignment.points_possible;
+}
+}
 }
 
-    ////LOOP THROUGH EACH SUBMISSION///
+return Object.values(learners).map(learner => {
+learner.avg = learner.totalScore/ learner.totalPoints;
+delete learner.totalScore;
+delete learner.totalPoints;
+return learner;
 
-for (let submission of submissions) {                          ///3///
-   let learnerId = submission.learner_id;
-   let assignmentId = submission.assignment_id;
-   let submittedScore = submission.submission.score;
-   let assignment = assignments[assignmentId]; 
-
-
-   let dueAt = new Date(assignment.due_at);
-   let maxPoints = assignment.points_possible;
-   return{dueAt, maxPoints};                 
-  }
-
-   let latePenalty = 0; 
-   if (submittedAt > dueAt) {
-       latePenalty = maxPoints * 0.1; 
-
-   }
-
-   let finalScore = Math.max(0, submittedScore - latePenalty);
-   let avgScore = FinalScore / maxPoints;
-
-
-  if (learners) {
-      learners = {id: learnerId, totalScore: 0, totalMax: 0};   ///get learners: id with average for assignment/////
-     return{learners}
-  }
+});
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////RETURN VALUE- WHEN WE RUN THE FUNCTION- IT SHOULD GIVE THIS RESULTS- BREAKOUT WITH OBJECTS//////////
-
-
-
-
-
-
-
-
-
-
-
+///////RETURN VALUE- WHEN WE RUN THE FUN
 ////////////////////////OUTPUT//////////////////////////////////  
-}
+
 
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-
 console.log(result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-
-//   console.log(result);
-
-
-
-
-
-///https://www.youtube.com/watch?v=ifi6dXOl3g4//
-
-
-
